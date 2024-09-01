b490a3ce2d29e55365b56b96a5ea8cd63c218dac
Abdulrhman Mohammed Shabaan <126196441+AbdulrhmanM0hamed@users.noreply.github.com>
2024-09-02 00:28:58 +0300

    add logo in splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/splash/presentation/view/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,8 +13,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: SplashView(),
+    return  GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(scaffoldBackgroundColor: kprimaryColor),
+      home:const SplashView(),
       
     );
   }
--- lib/splash/presentation/widget/splash_view_body.dart
+++ lib/splash/presentation/widget/splash_view_body.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: 
+    return  Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset('assets/images/Logo.png'),
+          )
+        ],
+      )
     );
   }
 }
