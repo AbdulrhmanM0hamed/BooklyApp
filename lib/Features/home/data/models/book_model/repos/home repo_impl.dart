722a527ae4995fc151b8578b1dec6f28b5ca921a
Abdulrhman Mohammed Shabaan <126196441+AbdulrhmanM0hamed@users.noreply.github.com>
2024-09-22 13:48:11 +0300

    finish failure

--- lib/Features/home/data/models/book_model/repos/home repo_impl.dart
+++ lib/Features/home/data/models/book_model/repos/home repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Features/home/data/models/book_model/repos/home_repo.
 import 'package:bookly_app/core/api_service.dart';
 import 'package:bookly_app/core/errors/failuer.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -10,23 +11,40 @@ class HomeRepoImpl implements HomeRepo {
   HomeRepoImpl(this.apiService);
 
   @override
-  Future<Either<Failuer, List<BookModel>>> fetchFutureBooks() {
-    throw UnimplementedError();
-  }
-
+  Future<Either<Failuer, List<BookModel>>> fetchFutureBooks() async {
+try {
+      final data = await apiService.get(endpoint: 'volumes?q=subject:programming&filtering=free-ebooks');
+      if (data['items'] != null) {
+        List<BookModel> books = (data['items'] as List).map((item) => BookModel.fromJson(item)).toList();
+        return Right(books);
+      } else {
+        return Left(ServerFailure(errMessage: "Something Error"));
+      }
+    } catch (e) {
+      if (e is DioException){
+          return Left(ServerFailure.fromDioError(e));
+      }
+    }
+    return Left(ServerFailure(errMessage: "Unexpected Error"));  
+    
+    
+    }
   @override
   Future<Either<Failuer, List<BookModel>>> fetchNewestBooks() async {
     try {
-      // طلب البيانات من الـ API
       final data = await apiService.get(endpoint: 'volumes?q=subject:programming&sorting=newest&filtering=free-ebooks');
       if (data['items'] != null) {
         List<BookModel> books = (data['items'] as List).map((item) => BookModel.fromJson(item)).toList();
         return Right(books);
       } else {
-        return Left(ServerFailure());
+        return Left(ServerFailure(errMessage: "Something Error"));
       }
     } catch (e) {
-      return Left(ServerFailure());
+      if (e is DioException){
+          return Left(ServerFailure.fromDioError(e));
+      }
     }
+    return Left(ServerFailure(errMessage: "Unexpected Error"));
+
   }
 }
